d2fa807f5b30cf79f7f5693252378e885516b044
Carlosmon04 <dcarlosmontoya04@gmail.com>
2024-02-04 20:49:26 -0600

    2;

---
+++ Problema2.dart
@@ -0,0 +1,26 @@
+void main (){
+
+final List<double > numeros = [1, 42,
+18,
+75,
+56,
+23,
+89,
+33,
+67,
+11,
+50];
+
+double suma = 0;
+
+for (double num  in numeros ){
+
+
+ suma = suma + num;
+
+
+}
+
+print(suma);
+
+}
