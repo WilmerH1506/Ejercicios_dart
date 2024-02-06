cbd46ac78906ff87674f612465f2f6a5176b8e45
WilmerH1506 <wilmerhy2005@gmail.com>
2024-02-06 02:04:21 -0600

    9

---
+++ Problema9.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  List <int> numeros = [10,25,5,14,2,6,95,7,6];
+  int suma = 0;
+
+  for (int i = 0; i < numeros.length; i++)
+  {
+    suma = suma + numeros[i];
+  }
+
+ print(suma);
+
+}
