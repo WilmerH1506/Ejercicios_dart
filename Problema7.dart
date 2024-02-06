5fe740570d4c61779d3f420d852979990e399e67
WilmerH1506 <wilmerhy2005@gmail.com>
2024-02-06 02:00:16 -0600

    7

--- Problema7.dart
+++ Problema7.dart
@@ -1,6 +1,6 @@
 void main()
 {
-   List <int> numeros = [100,10,7,8,3,21,6,95];
+   List <int> numeros = [100,10,7,8,3,21,6,95,70];
    int menor = numeros.first;
 
    for (int i = 0; i < numeros.length ; i++)
