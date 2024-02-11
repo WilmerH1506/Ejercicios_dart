02a98a8e00312c50106a0b08b1b07f82b226b86d
Carlosmon04 <dcarlosmontoya04@gmail.com>
2024-02-10 20:04:47 -0600

    nuevo 9

--- Problema9.dart
+++ Problema9.dart
@@ -1,17 +1,17 @@
 void main()
 {
   List <int> numeros = [10,25,5,14,2,6,95,7,6];
-  print(suma(numeros));
+  print(suma(datos: numeros));
 
 }
 
-int suma ( List <int> datos)
+int suma ( {required List<int> datos})
 {
   int suma = 0;
 
   for (int i = 0; i < datos.length; i++)
   {
-    suma = suma + datos[i];
+    suma += datos[i];
   }
 
   return suma;
